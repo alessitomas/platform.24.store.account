aea14e990a0ab3e4185c94ee64f7550ba00db1f6
marchettomarcelo <marcelovfm@al.insper.edu.br>
2024-05-15 10:39:52 -0300

    ok

--- src/main/java/insper/store/account/AccountController.java
+++ src/main/java/insper/store/account/AccountController.java
@@ -9,29 +9,32 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
 @FeignClient(name = "store-account")
 public interface AccountController {
 
     @PostMapping("/accounts")
     public ResponseEntity<AccountOut> create(
-        @RequestBody(required = true) AccountIn in
-    );
+            @RequestBody(required = true) AccountIn in);
 
     @PostMapping("/accounts/login")
     public ResponseEntity<AccountOut> login(
-        @RequestBody(required = true) LoginIn in
-    );
+            @RequestBody(required = true) LoginIn in);
 
     @PutMapping("/accounts/{id}")
     public ResponseEntity<AccountOut> update(
-        @PathVariable(required = true) String id,
-        @RequestBody(required = true) AccountIn in
-    );
+            @PathVariable(required = true) String id,
+            @RequestBody(required = true) AccountIn in);
 
+    @CircuitBreaker(name = "getAccounts", fallbackMethod = "fallbackGetAccounts")
     @GetMapping("/accounts")
     public ResponseEntity<AccountOut> read(
-        @RequestHeader(required = true, name = "id-user") String idUser
-        
+            @RequestHeader(required = true, name = "id-user") String idUser
+
     );
-    
+
+    public ResponseEntity<AccountOut> fallbackGetAccounts(
+            Throwable t);
+
 }
